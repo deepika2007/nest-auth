efd4c11f2436220cb3e29e4b192e20f75aa98336
Deepika Maheshwari <deepika200799@gmail.com>
2025-11-26 23:56:03 +0530

    fixed course udpate DTO

--- src/course/dto/update-course.dto.ts
+++ src/course/dto/update-course.dto.ts
@@ -1,4 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCourseDto } from './create-course.dto';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
-export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
+export class UpdateCourseDto extends PartialType(CreateCourseDto) {
+    @IsString()
+    name: string;
+
+    @IsString()
+    description: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    price: number;
+
+    @IsString()
+    level: string
+}
